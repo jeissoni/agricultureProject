40630025fee82d5d67b12ef91aae989f7742d147
jeissoni <jeissoni_22@hotmail.com>
2022-02-08 17:52:44 -0500

    add harvart property

--- contracts/Agriculture.sol
+++ contracts/Agriculture.sol
@@ -34,7 +34,8 @@ contract Agriculture {
         //precio por arbol
         uint256 priceTree;
         uint256 earningsTree;
-        //uint256 earningsTotalHarvest;
+       
+        uint256 earningsDepositFarmer;
         bool pause;
         stateHarvest state;
         address farmer;
